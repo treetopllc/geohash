36cd1cf7e9c9c1c28eb745d0695de505f087f808
Tom Burdick <thomas.burdick@gmail.com>
2012-10-10 16:20:56 -0500

    add nearby neighbor functionality for searching

--- src/geohash.erl
+++ src/geohash.erl
@@ -14,11 +14,21 @@
 
 %% @author Tom Burdick <thomas.burdick@gmail.com>
 %% @copyright 2012 Treetop Software LLC
-%% @doc GeoHash C Function Wrapper
+%% @doc GeoHash functions for Erlang with C implementations for core 
+%% functionality
+%% @end
 
 -module(geohash).
 
--export([decode/1, decode_bbox/1, encode/3, neighbor/2, neighbors/1, expand/1]).
+-export([
+    decode/1,
+    decode_bbox/1,
+    encode/3,
+    neighbor/2,
+    neighbors/1,
+    expand/1,
+    nearby/3
+]).
 
 -on_load(init/0).
 
@@ -60,6 +70,13 @@ neighbors(C) ->
 expand(C) ->
     [C | neighbors(C)].
 
+%% @doc Nearby geohashes useful for searching a region of a map
+-spec nearby(float(), float(), float()) -> [binary()].
+nearby(Lat, Lon, Rad) ->
+    Precision = nearby_precision(Lat, Lon, Rad),
+    GeoHash = encode(Lat, Lon, Precision),
+    expand(GeoHash).
+
 %% @private
 init() ->
     SoName = case code:priv_dir(?MODULE) of
@@ -79,4 +96,88 @@ init() ->
     _ -> ok
     end.
 
+%% @doc Best fit geohash precision
+-spec nearby_precision(float(), float(), float()) -> integer().
+nearby_precision(Lat, Lon, Rad) ->
+    {MinLat, MinLon, MaxLat, MaxLon} = earth_bounding_box(Lat, Lon, Rad),
+    DeltaLat = MaxLat - MinLat,
+    DeltaLon = MaxLon - MinLon,
+    Bits = geohash_bits(DeltaLat, DeltaLon, 63),
+    Bits/5.
+
+%% @doc Determine the bounding box of coordinates for a point and radius
+%% distance on the earth.
+%% @end
+-spec earth_bounding_box(float(), float(), float()) ->
+    {float(), float(), float(), float()}. 
+earth_bounding_box(Lat, Lon, Dist)  ->
+    MIN_LAT = radians(-90),
+    MAX_LAT = radians(90),
+    MIN_LON = radians(-180),
+    MAX_LON = radians(180),
+    AngularDist  = Dist / earth_radius(),
+    Lat0 = radians(Lat),
+    Lon0 = radians(Lon),
+    MinLat = Lat0 - AngularDist,
+    MaxLat = Lat0 - AngularDist,
+    case (MinLat > MIN_LAT) and (MaxLat < MAX_LAT) of
+        true ->
+            DeltaLon = math:asin(math:sin(AngularDist) / math:cos(Lat0)),
+            MinLon = Lon0 - DeltaLon,
+            MinLon0 = case MinLon < MIN_LON of
+                true ->
+                    MinLon + 2.0*math:pi();
+                false ->
+                    MinLon
+            end,
+            MaxLon = Lon0 + DeltaLon,
+            MaxLon0 = case MaxLon > MAX_LON of
+                true ->
+                    MaxLon - 2.0*math:pi();
+                false ->
+                    MaxLon
+            end,
+            {degrees(MinLat), degrees(MinLon0),
+                degrees(MaxLat), degrees(MaxLon0)};
+        false ->
+            MinLat0 = math:max(MinLat, MIN_LAT),
+            MaxLat0 = math:min(MaxLat, MAX_LAT),
+            {degrees(MinLat0), degrees(MIN_LON),
+                degrees(MaxLat0), degrees(MAX_LON)}
+    end.
+
+%% @doc Earth Radius
+-spec earth_radius() -> float().
+earth_radius() ->
+    6371.0.
+
+%% @doc Convert degrees to radians
+-spec radians(float()) -> float().
+radians(Degrees) ->
+    (math:pi()/180.0)*Degrees.
+
+%% @doc Convert radians to degrees
+-spec degrees(float()) -> float().
+degrees(Radians) ->
+    (180.0/math:pi())*Radians.
+
+%% @doc Determine the number of bits required (slices) to encompass a 
+%% latitude and longitude range.
+geohash_bits(_, _, 0) ->
+    0;
+geohash_bits(DeltaLat, DeltaLon, Bits) ->
+    case (delta_lat(Bits) < DeltaLat) or (delta_lon(Bits) < DeltaLon) of
+        true ->
+            geohash_bits(DeltaLat, DeltaLon, Bits-1);
+        false ->
+            Bits
+    end.
+
+%% @doc Give the delta for a number of bits used in geohashing latitude
+delta_lat(Bits) ->
+    180.0 / math:pow(2, Bits / 2).
+
+%% @doc Give the delta for a number of bits used in geohashing longitude
+delta_lon(Bits) ->
+    360.0 / math:pow(2, (Bits + 1) / 2).
 
